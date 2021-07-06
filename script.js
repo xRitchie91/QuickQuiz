2c982149b1e27791c4b05a177379329e51e0ec77
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-05 22:07:29 -0500

    added quiz functions

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Quiz functions 
-
+let topScores = document.querySelector("#topscores");
+let countdown = document.querySelector("#countdown");
+let content = document.querySelector("#content");
+let quizTime = document.querySelector("#quiztime");
 
 
 // Event listeners/Buttons
-scores.addEventListener("click", scores);
+topScores.addEventListener("click", topScores);
