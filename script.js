1ce9eed07e9d7071ee82980680042e88bb30565d
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-05 23:31:29 -0500

    updated script file
    added init function 
    added instructions element

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-// Quiz functions 
+const { reset } = require("yargs");
+
+// Quiz page objects
 let topScores = document.querySelector("#topscores");
 let countdown = document.querySelector("#countdown");
-let content = document.querySelector("#content");
+let mainContent = document.querySelector("#maincontent");
 let quizTime = document.querySelector("#quiztime");
+
+// variables
+var test = false;
+var score = 0;
+var quiz = {};
+
+var quizCountdown = 10;
+var quizElapsedTime = 0;
+var quizIntervals;
+
+init();
+
+// shows the instructions on the main page
+function init() {
+    detailsReset();
+    reset();
+
+    // heading
+    let heading = document.createElement("instructions");
+    heading.setAttribute("id", "heading");
+    heading.textContent = "QuickQuiz is a timed quiz game."
+}
 
 
 // Event listeners/Buttons
