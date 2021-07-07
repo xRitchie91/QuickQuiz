ea6904aec3e6be7ca1d9ccde3b418b9b09cf5877
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-06 19:55:02 -0500

    removed questions from script.js to questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -1,1 +1,38 @@
 // Quiz questions will go here
+var questions = [
+    {
+        title: "What does the 'this' keyword in javascript mean?",
+        choices: ["the current object", "booleans", "cursor", "numbers"],
+        answer: "the current object"
+    },
+    {
+        title: "String values must be enclosed with ___ when being assigned to variables?",
+        choices: ["curly brackets", "objects", "threads", "parenthesis"],
+        answer: "curly brackets"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        choices: ["react", "python", "alerts", "for loops"],
+        answer: "for loops"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "using a/an ___ statement is how you test for a specific condition",
+        choices: ["switch","for","if","select"],
+        answer: "if"
+    },
+    {
+        title: "which is not considered a javascript operator?",
+        choices: ["delete", "typeof", "this", "new"],
+        answer: "this"
+    },
+    {
+        title: "arrays in javascrips can be used to store what?",
+        choices: ["booleans", "other arrays", "numbers and strings", "all of the above"],
+        answer: "all of the above"
+    }
+];
